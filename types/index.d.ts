eba500f245708486babdb47e93f346c62e5d4f3f
Guillaume GARCIA <guillaume.garcia13@gmail.com>
2021-03-15 22:29:23 +0100

    adds type definitions

---
+++ types/index.d.ts
@@ -0,0 +1,26 @@
+// TypeScript Version: 3.5
+
+import { Node } from 'unist';
+import { Test } from 'unist-util-is';
+
+declare namespace remove {
+  interface RemoveOptions {
+    cascade?: boolean;
+  }
+}
+
+/**
+ *
+ * Mutate the given tree by removing all nodes that pass test. The tree is walked in preorder (NLR), visiting the node itself, then its head, etc.
+ *
+ * @param tree      Tree to filter
+ * @param options   Whether to drop parent nodes if they had children, but all their children were filtered out
+ * @param test      is-compatible test (such as a type)
+ */
+declare function remove(
+  tree: Node,
+  options: remove.RemoveOptions = {cascade: true},
+  test?: Test<Node>
+): Node;
+
+export = remove;
