6baa5dbf2bd549972accf6150e3b8330b40d95f5
Guillaume GARCIA <guillaume.garcia13@gmail.com>
2021-04-02 00:09:48 +0200

    adds type test

--- types/index.d.ts
+++ types/index.d.ts
@@ -17,14 +17,19 @@ declare namespace remove {
  * Mutate the given tree by removing all nodes that pass test. The tree is walked in preorder (NLR), visiting the node itself, then its head, etc.
  *
  * @param tree      Tree to filter
- * @param options   Whether to drop parent nodes if they had children, but all their children were filtered out
  * @param test      is-compatible test (such as a type)
  */
-declare function remove(tree: Node, test?: Test<Node>): Node
-declare function remove(
+export function remove(tree: Node, test?: Test<Node>): Node
+/**
+ *
+ * Mutate the given tree by removing all nodes that pass test. The tree is walked in preorder (NLR), visiting the node itself, then its head, etc.
+ *
+ * @param tree      Tree to filter
+ * @param options   Whether to drop parent nodes if they had children, but all their children were filtered out. Default is {cascade: true}
+ * @param test      is-compatible test (such as a type)
+ */
+export function remove(
   tree: Node,
-  options: remove.RemoveOptions = {cascade: true},
+  options?: remove.RemoveOptions,
   test?: Test<Node>
 ): Node
-
-export = remove
