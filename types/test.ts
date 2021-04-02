17f21674f4ac1f7641f95e94f06e19e12239b21b
GARCIA Guillaume <guillaume.garcia13@gmail.com>
2021-04-02 11:51:04 +0200

    Add types (#3)
    Closes GH-3.
    
    Reviewed-by: Christian Murphy <christian.murphy.42@gmail.com>
    Reviewed-by: Remco Haszing <remcohaszing@gmail.com>
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

---
+++ types/test.ts
@@ -0,0 +1,19 @@
+import * as remove from 'uninst-util-remove'
+import * as u from 'unist-builder'
+
+const tree = u('root', [
+  u('leaf', '1'),
+  u('node', [
+    u('leaf', '2'),
+    u('node', [u('leaf', '3'), u('other', '4')]),
+    u('node', [u('leaf', '5')])
+  ]),
+  u('leaf', '6')
+])
+
+remove() // $ExpectError
+remove('leaf') // $ExpectError
+
+remove(tree)
+remove(tree, 'leaf')
+remove(tree, {cascade: false}, 'leaf')
