b2d67dd7bd9eaca9af55d4d73a5872795389b9f9
Titus Wormer <tituswormer@gmail.com>
2023-07-03 15:52:21 +0200

    Change to use `export` map

--- test.js
+++ test.js
@@ -6,11 +6,13 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {u} from 'unist-builder'
-import {remove} from './index.js'
+import {remove} from 'unist-util-remove'
 
 test('remove', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('./index.js')).sort(), ['remove'])
+    assert.deepEqual(Object.keys(await import('unist-util-remove')).sort(), [
+      'remove'
+    ])
   })
 
   await t.test('should compare nodes by partial properties', function () {
